757b9d0a37c0f9b7080b2d804ca08c2738fc3887
Mike <mike@mikera.net>
2019-04-02 08:41:30 +0800

    Byte to string conversion tests

--- src/test/clojure/starfish/test_core.clj
+++ src/test/clojure/starfish/test_core.clj
@@ -1,3 +1,13 @@
 (ns starfish.test-core
-  (:require [clojure.test :refer [is testing]]))
+  (:require [clojure.test :refer [is testing deftest run-all-tests]])
+  (:require [starfish.core :refer :all]))
 
+(deftest test-byte-strings
+  (is (= "Foo" (to-string (to-bytes "Foo"))))
+  (is (= "Foo" (to-string (to-string (to-bytes "Foo")))))
+  (is (= "Foo" (to-string (to-bytes (to-bytes "Foo"))))))
+
+
+(comment
+  (run-all-tests)
+  )
