16d71c618b2a94bb8561aebb79734fc5ea2bc7b4
shark8me <kiran.karkera@gmail.com>
2019-04-10 18:26:26 +0800

    resolves #3

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject sg.dex/starfish-clj "0.0.1-SNAPSHOT"
   :url "https://github.com/DEX-Company/starfish-clj"
   :dependencies [[sg.dex/starfish-java "0.0.1-SNAPSHOT"]
+                 [org.slf4j/jcl-over-slf4j "1.7.26"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.csv "0.1.4"]]
+  :exclusions [commons-logging/commons-logging]
   :managed-dependencies [[com.fasterxml.jackson.core/jackson-databind "2.9.8"]]
 
   :javac-options ["-target" "1.8", "-source" "1.8"]
@@ -11,7 +13,6 @@
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
   :test-selectors {:default (constantly true)}
-
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]
                    ;; :resource-paths ["src/main/resources"]
                    }
