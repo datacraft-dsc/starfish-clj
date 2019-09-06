9c507c90eec556c9d91956b93a46d211de9a7eef
AyushVerma2 <ayushverma2@gmail.com>
2019-09-06 17:04:22 +0800

    skipping squid test

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sg.dex/starfish-clj "0.0.7-SNAPSHOT"
+(defproject sg.dex/starfish-clj "0.6.0-SNAPSHOT"
   :url "https://github.com/DEX-Company/starfish-clj"
   :dependencies [[sg.dex/starfish-java "0.7.0"]
                  [org.slf4j/jcl-over-slf4j "1.8.0-alpha2"]
