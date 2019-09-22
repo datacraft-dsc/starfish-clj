2d2139bc25b802144555cc003fd74d348b790d9c
Mike <mike@mikera.net>
2019-09-22 12:06:11 +0800

    Update to starfish-java 0.7.4

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sg.dex/starfish-clj "0.6.0"
   :url "https://github.com/DEX-Company/starfish-clj"
-  :dependencies [[sg.dex/starfish-java "0.7.3"]
+  :dependencies [[sg.dex/starfish-java "0.7.4"]
                  [org.clojure/data.json "0.2.6"]
                  [clojurewerkz/propertied "1.3.0"]
                  [org.clojure/data.csv "0.1.4"]
