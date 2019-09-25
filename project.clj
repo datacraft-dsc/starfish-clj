4ab0ebaee1d2507da5715cf9845fd49b1ff1e97c
Mike <mike@mikera.net>
2019-09-25 10:44:52 +0800

    Update to 0.7.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sg.dex/starfish-clj "0.7.3-SNAPSHOT"
+(defproject sg.dex/starfish-clj "0.7.3"
   :url "https://github.com/DEX-Company/starfish-clj"
   :dependencies [[sg.dex/starfish-java "0.7.7"]
                  [org.clojure/data.json "0.2.6"]
