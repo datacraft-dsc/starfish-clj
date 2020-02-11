b79c67c6c88214b70113369acf9641c4916e37c5
Mike <mike@mikera.net>
2020-02-11 11:52:05 +0800

    Update dependencies, add lein-ancient plugin

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject sg.dex/starfish-clj "0.7.7"
   :url "https://github.com/DEX-Company/starfish-clj"
   :dependencies [[sg.dex/starfish-java "0.8.0"]
-                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/data.json "0.2.7"]
                  [org.clojure/data.csv "0.1.4"]
                  [clojurewerkz/propertied "1.3.0"]
 
                  ;; Used to fix JCL issues with Apache HTTP logging via JCL
-                 [org.slf4j/jcl-over-slf4j "1.7.28"]]
+                 [org.slf4j/jcl-over-slf4j "1.7.30"]]
 
   ;; :javac-options ["-target" "8", "-source" "8"] ; TODO figure out of this is helpful? Causes a warning
   :target-path "target/%s/"
@@ -15,7 +15,8 @@
   :test-paths ["src/test/clojure" "src/test/java"]
   :test-selectors {:default (complement :integration)
                    :integration :integration}
-  :plugins [[lein-codox "0.10.7"]]
+  :plugins [[lein-codox "0.10.7"]
+            [lein-ancient "0.6.15"]]
   :codox {:output-path "codox"}
   :min-lein-version "2.8.1"
   :lein-release {:deploy-via :clojars}
